7f8a6f26a6e9a48b1e61deb9ca5be6fbb0d0aa67
Herbert Verdida <hjverdida@gmail.com>
2019-03-29 08:52:11 +0800

    Modularized

---
+++ helpers.py
@@ -0,0 +1,15 @@
+import re
+import functools
+
+ALPHANUM_RE = re.compile(r'[^a-z0-9]')
+
+
+def to_lowercased_alphanum(text):
+    return ALPHANUM_RE.sub('', text.lower())
+
+
+def compose(*functions):
+    def _compose(f, g):
+        return lambda arg: g(f(arg))
+
+    return functools.reduce(_compose, functions, lambda arg: arg)
