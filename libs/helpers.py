9d4b378be2d1a78d6db218cf2e0a7e3ca7b9111d
Herbert Verdida <hjverdida@gmail.com>
2019-04-01 09:58:38 +0800

    Refactor: removed alphanum regex from global

--- libs/helpers.py
+++ libs/helpers.py
@@ -1,12 +1,10 @@
 import re
 import functools
 
-ALPHANUM_RE = re.compile(r'[^a-z0-9]')
 
+def to_lowercased_alphanum(text, ):
 
-def to_lowercased_alphanum(text):
-
-    return ALPHANUM_RE.sub('', text.lower())
+    return re.sub(r'[^a-z0-9]', '', text.lower())
 
 
 def compose(*functions):
